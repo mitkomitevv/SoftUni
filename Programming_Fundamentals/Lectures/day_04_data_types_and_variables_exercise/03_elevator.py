b090124815aa8fb7257550393a22cfd62feffe33
Mitko Mitev <onesible@gmail.com>
2023-09-28 14:01:00 +0300

    Update 03_elevator.py

--- Programming_Fundamentals/Lectures/day_04_data_types_and_variables_exercise/03_elevator.py
+++ Programming_Fundamentals/Lectures/day_04_data_types_and_variables_exercise/03_elevator.py
@@ -4,7 +4,4 @@ courses = people // capacity
 courses_2 = people % capacity
 if courses_2 > 0:
     courses += 1
-if capacity >= people:
-    print("All the persons fit inside the elevator. Only one course is needed.")
-else:
-    print(courses)
+print(courses)
