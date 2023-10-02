0b8640917b2e8495ede396e275dceaeda85f4632
Mitko Mitev <onesible@gmail.com>
2023-10-02 19:06:50 +0300

    Update 02_prime_number_checker.py

--- Programming_Fundamentals/Exercises/data_types_and_variables_more_exercises/02_prime_number_checker.py
+++ Programming_Fundamentals/Exercises/data_types_and_variables_more_exercises/02_prime_number_checker.py
@@ -1,9 +1,10 @@
 number = int(input())
 counter = 0
-for num in range(2, number + 1):
+for num in range(2, number):
     if number % num == 0:
         counter += 1
-if counter > 1:
+        break
+if counter > 0:
     print('False')
 else:
     print('True')
