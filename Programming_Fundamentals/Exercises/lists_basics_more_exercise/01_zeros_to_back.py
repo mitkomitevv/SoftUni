0bc1cf1873937655faaaaefb8f8aae88ff9de0e5
Mitko Mitev <onesible@gmail.com>
2023-10-02 21:36:55 +0300

    Update 01_zeros_to_back.py

--- Programming_Fundamentals/Exercises/lists_basics_more_exercise/01_zeros_to_back.py
+++ Programming_Fundamentals/Exercises/lists_basics_more_exercise/01_zeros_to_back.py
@@ -2,9 +2,9 @@ strings = input().split(', ')
 str_to_int = [int(x) for x in strings]
 count_0 = str_to_int.count(0)
 removed_0 = [x for x in str_to_int if x != 0]
-formatted = removed_0
-for i in range(count_0):
-    formatted.append(0)
+formatted = removed_0 + [0] * count_0
 print(formatted)
-# formatted = [y[0] for y in range(count_0)]
+# formatted = removed_0
+# for i in range(count_0):
+#     formatted.append(0)
 # print(formatted)
