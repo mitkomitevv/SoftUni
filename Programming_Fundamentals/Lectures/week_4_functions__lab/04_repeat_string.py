d48f7fdd981d639c1fd111da649e951eae82053b
Mitko Mitev <onesible@gmail.com>
2023-10-03 15:38:21 +0300

    Create 04_repeat_string.py

---
+++ Programming_Fundamentals/Lectures/week_4_functions__lab/04_repeat_string.py
@@ -0,0 +1,11 @@
+string = input()
+counter = int(input())
+print((lambda x: string * counter)(counter))
+
+
+# def formatted():
+#     result = string * counter
+#     return result
+#
+#
+# print(formatted())
