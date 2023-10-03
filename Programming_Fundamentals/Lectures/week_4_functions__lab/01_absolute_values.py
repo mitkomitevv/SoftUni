8d19021ec3ac40e06b144b55cc39ebec38b39929
Mitko Mitev <onesible@gmail.com>
2023-10-03 15:08:17 +0300

    Update 01_absolute_values.py

--- Programming_Fundamentals/Lectures/week_4_functions__lab/01_absolute_values.py
+++ Programming_Fundamentals/Lectures/week_4_functions__lab/01_absolute_values.py
@@ -1,6 +1,6 @@
 def formatted():
     sequence = input().split(' ')
-    numbers = [float(x) for (x) in sequence]
+    numbers = [float(x) for x in sequence]
     formatted_numbers = [abs(y) for y in numbers]
     return formatted_numbers
 
