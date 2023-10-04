9bb13a68fecd3675ca8af572c6ea7e33c87951b6
Mitko Mitev <onesible@gmail.com>
2023-10-04 14:17:10 +0300

    Update 01_smallest_of_three_numbers.py

--- Programming_Fundamentals/Lectures/week_4_functions_exercise/01_smallest_of_three_numbers.py
+++ Programming_Fundamentals/Lectures/week_4_functions_exercise/01_smallest_of_three_numbers.py
@@ -4,12 +4,14 @@ n_3 = int(input())
 
 
 def smallest():
-    if n_1 < n_2 and n_1 < n_3:
-        return n_1
-    elif n_2 < n_1 and n_2 < n_3:
-        return n_2
-    else:
-        return n_3
+    smallest_number = min(n_1, n_2, n_3)
+    return smallest_number
+    # if n_1 < n_2 and n_1 < n_3:
+    #     return n_1
+    # elif n_2 < n_1 and n_2 < n_3:
+    #     return n_2
+    # else:
+    #     return n_3
 
 
 print(smallest())
