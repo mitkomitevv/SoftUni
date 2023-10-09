62dbc3ab2b2511a19b425ee495512141b98a6350
Mitko Mitev <onesible@gmail.com>
2023-10-09 20:18:02 +0300

    Create trains.py

---
+++ Programming_Fundamentals/Lectures/week_5_lists_advanced/lab/trains.py
@@ -0,0 +1,22 @@
+def train(num):
+    train_list = []
+    for _ in range(num):
+        train_list.append(0)
+    input_line = input()
+    while input_line != 'End':
+        command = input_line.split()
+        counter = 0
+        if command[0] == 'add':
+            popped = train_list.pop()
+            counter += int(command[1]) + int(popped)
+            train_list.append(counter)
+        elif command[0] == 'insert':
+            train_list[int(command[1])] = int(command[2]) + train_list[int(command[1])]
+        elif command[0] == 'leave':
+            train_list[int(command[1])] = train_list[int(command[1])] - int(command[2])
+        input_line = input()
+    return train_list
+
+
+number = int(input())
+print(train(number))
