5556b8fdabe91ca35eeef10dbf8575384066bf41
Mitko Mitev <onesible@gmail.com>
2023-10-13 19:45:14 +0300

    Update office_chairs.py

--- Programming_Fundamentals/Lectures/week_5_lists_advanced/exercise/office_chairs.py
+++ Programming_Fundamentals/Lectures/week_5_lists_advanced/exercise/office_chairs.py
@@ -1,18 +1,16 @@
 def enough_chairs(num):
     free_chairs = 0
-    room_number = 1
     flag = False
-    for _ in range(num):
+    for n in range(1, num + 1):
         string = input().split()
         chairs = len(string[0])
         visitors = int(string[1])
         if visitors > chairs:
             flag = True
             needed_chairs = visitors - chairs
-            print(f'{needed_chairs} more chairs needed in room {room_number}')
+            print(f'{needed_chairs} more chairs needed in room {n}')
         else:
             free_chairs += chairs - visitors
-        room_number += 1
     if not flag:
         print(f'Game On, {free_chairs} free chairs left')
 
