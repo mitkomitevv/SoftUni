de31a0b145db6c3ed5d96b61bf82464d72395b16
Mitko Mitev <onesible@gmail.com>
2023-10-14 23:29:39 +0300

    Update softuni_course_planning.py

--- Programming_Fundamentals/Lectures/week_5_lists_advanced/exercise/softuni_course_planning.py
+++ Programming_Fundamentals/Lectures/week_5_lists_advanced/exercise/softuni_course_planning.py
@@ -3,18 +3,18 @@ schedule = initial_schedule.copy()
 line2 = input()
 while line2 != 'course start':
     command = line2.split(':')
-    if command[0] == 'Add':
+    if 'Add' in command:
         if command[1] not in schedule:
             schedule.append(command[1])
     elif command[0] == 'Insert':
         if command[1] not in schedule:
             schedule.insert(int(command[2]), command[1])
-    elif command[0] == 'Remove':
+    elif 'Remove' in command:
         if command[1] in schedule:
             schedule.remove(command[1])
             if f'{command[1]}-Exercise' in schedule:
                 schedule.remove(f'{command[1]}-Exercise')
-    elif command[0] == 'Swap':
+    elif 'Swap' in command:
         if command[1] in schedule and command[2] in schedule:
             lesson1_i = schedule.index(command[1])
             lesson2_i = schedule.index(command[2])
@@ -27,7 +27,7 @@ while line2 != 'course start':
                 for_insert = schedule.index(command[2])
                 schedule.remove(f'{command[2]}-Exercise')
                 schedule.insert(for_insert + 1, f'{command[2]}-Exercise')
-    elif command[0] == 'Exercise':
+    elif 'Exercise' in command:
         if command[1] in schedule and f'{command[1]}-Exercise' not in schedule:
             lesson_i = schedule.index(command[1])
             schedule.insert(lesson_i + 1, f'{command[1]}-Exercise')
@@ -37,7 +37,7 @@ while line2 != 'course start':
     line2 = input()
 for i, lesson in enumerate(schedule, start=1):
     print(f'{i}.{lesson}')
-    
+
 
 # initial_schedule = input().split(', ')
 # schedule = initial_schedule.copy()
