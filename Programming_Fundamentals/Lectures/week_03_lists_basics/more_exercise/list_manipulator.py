4e7470dfe4381d16aa41923cae9189eef5df73f8
Mitko Mitev <onesible@gmail.com>
2023-10-19 21:08:47 +0300

    Update list_manipulator.py

--- Programming_Fundamentals/Lectures/week_03_lists_basics/more_exercise/list_manipulator.py
+++ Programming_Fundamentals/Lectures/week_03_lists_basics/more_exercise/list_manipulator.py
@@ -1,60 +1,70 @@
-data = [int(x) for x in input().split()]
-input_line = input()
-left, right = 0, 0
+def exchange(nums, idx):
+    if 0 <= idx < len(nums):
+        nums = nums[idx + 1:] + nums[:idx + 1]
+    else:
+        print('Invalid index')
+    return nums
 
-while input_line != 'end':
-    command = input_line.split()
-    even_numbers = [x for x in data if x % 2 == 0]
-    odd_numbers = [x for x in data if x % 2 != 0]
+def max_func(cmd, nums, even_nums, odd_nums):
+    if 'even' in cmd and even_nums:
+        max_even_index = len(nums) - nums[::-1].index(max(even_nums)) - 1
+        print(max_even_index)
+    elif 'odd' in cmd and odd_nums:
+        max_odd_index = len(nums) - nums[::-1].index(max(odd_nums)) - 1
+        print(max_odd_index)
+    else:
+        print('No matches')
 
-    if 'exchange' in command:
-        if len(data) > int(command[1]) >= 0:
-            left, right = data[:int(command[1]) + 1], data[int(command[1]) + 1:]
-            data = right + left
+def min_func(cmd, nums, even_nums, odd_nums):
+    if 'even' in cmd and even_nums:
+        min_even_index = len(nums) - nums[::-1].index(min(even_nums)) - 1
+        print(min_even_index)
+    elif 'odd' in cmd and odd_nums:
+        min_odd_index = len(nums) - nums[::-1].index(min(odd_nums)) - 1
+        print(min_odd_index)
+    else:
+        print('No matches')
+
+def first(cmd, nums, even_nums, odd_nums, count):
+    if 0 < count <= len(nums):
+        if 'even' in cmd:
+            first_count_even = even_nums[:count]
+            print(first_count_even)
         else:
-            print('Invalid index')
+            first_count_odd = odd_nums[:count]
+            print(first_count_odd)
+    else:
+        print(f"Invalid count")
 
-    elif 'max' in command:
-        if 'odd' in command and odd_numbers:
-            max_odd_i = len(data) - data[::-1].index(max(odd_numbers)) - 1
-            print(max_odd_i)
-        elif 'even' in command and even_numbers:
-            max_even_i = len(data) - data[::-1].index(max(even_numbers)) - 1
-            print(max_even_i)
+def last(cmd, nums, even_nums, odd_nums, count):
+    if 0 < count <= len(nums):
+        if 'even' in cmd:
+            first_count_even = even_nums[-count:]
+            print(first_count_even)
         else:
-            print('No matches')
+            first_count_odd = odd_nums[-count:]
+            print(first_count_odd)
+    else:
+        print(f"Invalid count")
 
-    elif 'min' in command:
-        if 'odd' in command and odd_numbers:
-            min_odd_i = len(data) - data[::-1].index(min(odd_numbers)) - 1
-            print(min_odd_i)
-        elif 'even' in command and even_numbers:
-            min_even_i = len(data) - data[::-1].index(min(even_numbers)) - 1
-            print(min_even_i)
-        else:
-            print('No matches')
-
-    elif 'first' in command:
-        if int(command[1]) <= len(data):
-            if 'odd' in command:
-                first_count_odd = odd_numbers[:int(command[1])]
-                print(first_count_odd)
-            elif 'even' in command:
-                first_count_even = even_numbers[:int(command[1])]
-                print(first_count_even)
-        else:
-            print('Invalid count')
-
-    elif 'last' in command:
-        if int(command[1]) <= len(data):
-            if 'odd' in command:
-                last_count_odd = odd_numbers[-int(command[1]):]
-                print(last_count_odd)
-            elif 'even' in command:
-                last_count_even = even_numbers[-int(command[1]):]
-                print(last_count_even)
-        else:
-            print('Invalid number')
+def main():
+    numbers = [int(x) for x in input().split()]
     input_line = input()
-
-print(data)
+    while input_line != 'end':
+        command = input_line.split()
+        even = [x for x in numbers if x % 2 == 0]
+        odd = [x for x in numbers if x % 2 != 0]
+        if 'exchange' in command:
+            index = int(command[1])
+            numbers = exchange(numbers, index)
+        elif 'max' in command:
+            max_func(command, numbers, even, odd)
+        elif 'min' in command:
+            min_func(command, numbers, even, odd)
+        elif 'first' in command:
+            first(command, numbers, even, odd, int(command[1]))
+        elif 'last' in command:
+            last(command, numbers, even, odd, int(command[1]))
+        input_line = input()
+    return numbers
+print(main())
