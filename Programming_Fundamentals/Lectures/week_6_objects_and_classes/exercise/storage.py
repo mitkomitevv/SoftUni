da457b7406587770eab16a18707a1004e8cc45b1
Mitko Mitev <onesible@gmail.com>
2023-10-27 15:11:57 +0300

    Create storage.py

---
+++ Programming_Fundamentals/Lectures/week_6_objects_and_classes/exercise/storage.py
@@ -0,0 +1,13 @@
+class Storage:
+
+    storage = []
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+    def add_product(self, product: str):
+        if len(self.storage) < self.capacity:
+            Storage.storage.append(product)
+
+    def get_products(self):
+        return self.storage
