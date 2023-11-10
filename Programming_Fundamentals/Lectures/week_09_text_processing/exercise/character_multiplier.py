89c5cf7718c36ac1eb5e2100417176ae8557898d
Mitko Mitev <onesible@gmail.com>
2023-11-10 13:44:11 +0200

    Update character_multiplier.py

---
+++ Programming_Fundamentals/Lectures/week_09_text_processing/exercise/character_multiplier.py
@@ -0,0 +1,27 @@
+string_list = input().split()
+total_sum = 0
+str1, str2 = string_list[0], string_list[1]
+num1, num2 = 0, 0
+while True:
+    for char in str1:
+        num1 = ord(char)
+        break
+
+    for char in str2:
+        num2 = ord(char)
+        break
+
+    if str1 and str2:
+        total_sum += num1 * num2
+    elif str1:
+        total_sum += num1
+    elif str2:
+        total_sum += num2
+
+    if not str1 and not str2:
+        break
+
+    str1 = str1[1::]
+    str2 = str2[1::]
+
+print(total_sum)
