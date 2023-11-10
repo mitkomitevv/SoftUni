4e2550cfb196e39f699a3e3c82f719a42573393b
Mitko Mitev <onesible@gmail.com>
2023-11-10 13:44:47 +0200

    Create digits_letters_and_other.py

---
+++ Programming_Fundamentals/Lectures/week_09_text_processing/lab/digits_letters_and_other.py
@@ -0,0 +1,13 @@
+string = input()
+digits, letters, other = '', '', ''
+for char in string:
+    if char.isdigit():
+        digits += char
+    elif char.isalpha():
+        letters += char
+    else:
+        other += char
+
+print(digits)
+print(letters)
+print(other)
