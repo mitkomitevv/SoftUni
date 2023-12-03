4abed47d195c00730063c436cea7ab96078f8d11
Mitko Mitev <onesible@gmail.com>
2023-12-03 19:35:38 +0200

    Update sorting_names.py

--- Programming_Fundamentals/Lectures/week_05_lists_advanced/lab/sorting_names.py
+++ Programming_Fundamentals/Lectures/week_05_lists_advanced/lab/sorting_names.py
@@ -5,3 +5,12 @@ def sort_names(names):
 
 input_names = input().split(', ')
 print(sort_names(input_names))
+
+
+
+# def sort_names():
+#     name = input().split(", ")
+#
+#     return sorted(name, key=lambda x: (-len(x), x))
+#
+# print(sort_names())
