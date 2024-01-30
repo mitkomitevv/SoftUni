23b064c5d5d11acc18c632fc8f70a221faa15188
Mitko Mitev <onesible@gmail.com>
2024-01-30 12:00:59 +0200

    Create offroad_challenge.py

---
+++ Python_Advanced/Python_Advanced/Exercises/exam_2023_10_22/offroad_challenge.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+fuel_quantities = deque(int(n) for n in input().split())
+consumption = deque(int(n) for n in input().split())
+quantity = deque(int(n) for n in input().split())
+reached, len_quantity = 0, len(quantity)
+
+while fuel_quantities:
+    fuel, index, needed_fuel = fuel_quantities.pop(), consumption.popleft(), quantity.popleft()
+    result = fuel - index
+
+    if result >= needed_fuel:
+        reached += 1
+    else:
+        break
+
+if reached > 0:
+    [print(f"John has reached: Altitude {i}") for i in range(1, reached + 1)]
+if reached < len_quantity:
+    print(f"John did not reach: Altitude {len_quantity - len(quantity)}")
+if reached and reached < len_quantity:
+    print(f"John failed to reach the top.")
+    print(f"Reached altitudes: {', '.join(f'Altitude {i}' for i in range(1, reached + 1))}")
+else:
+    print(f"John failed to reach the top.\n"
+          f"John didn't reach any altitude.")
+if reached == len_quantity:
+    print("John has reached all the altitudes and managed to reach the top!")
