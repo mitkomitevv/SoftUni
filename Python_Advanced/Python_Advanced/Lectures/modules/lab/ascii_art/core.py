bd278bb3bad65ae52221448698fb800b090c44b8
Mitko Mitev <onesible@gmail.com>
2024-02-07 13:44:21 +0200

    adding solution

---
+++ Python_Advanced/Python_Advanced/Lectures/modules/lab/ascii_art/core.py
@@ -0,0 +1,4 @@
+from pyfiglet import figlet_format
+
+def print_art(string):
+    print(figlet_format(string))
---
+++ Python_Advanced/Python_Advanced/Lectures/modules/lab/ascii_art/main.py
@@ -0,0 +1,5 @@
+from Python_Advanced.Python_Advanced.Lectures.modules.lab.ascii_art.core import print_art
+
+string =  input()
+
+print_art(string)
