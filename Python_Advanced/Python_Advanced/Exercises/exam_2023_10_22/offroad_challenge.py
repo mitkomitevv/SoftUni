3587401df076463bef753f8765ccf1764ab2894d
Mitko Mitev <onesible@gmail.com>
2024-02-13 12:57:17 +0200

    Update offroad_challenge.py

--- Python_Advanced/Python_Advanced/Exercises/exam_2023_10_22/offroad_challenge.py
+++ Python_Advanced/Python_Advanced/Exercises/exam_2023_10_22/offroad_challenge.py
@@ -3,7 +3,7 @@ from collections import deque
 fuel_quantities = deque(int(n) for n in input().split())
 consumption = deque(int(n) for n in input().split())
 quantity = deque(int(n) for n in input().split())
-reached, len_quantity = 0, len(quantity)
+reached, initial_len_quantity = 0, len(quantity)
 
 while fuel_quantities:
     fuel, index, needed_fuel = fuel_quantities.pop(), consumption.popleft(), quantity.popleft()
@@ -16,13 +16,13 @@ while fuel_quantities:
 
 if reached > 0:
     [print(f"John has reached: Altitude {i}") for i in range(1, reached + 1)]
-if reached < len_quantity:
-    print(f"John did not reach: Altitude {len_quantity - len(quantity)}")
-if reached and reached < len_quantity:
+if reached < initial_len_quantity:
+    print(f"John did not reach: Altitude {initial_len_quantity - len(quantity)}")
+if reached == initial_len_quantity:
+    print("John has reached all the altitudes and managed to reach the top!")
+elif reached and reached < initial_len_quantity:
     print(f"John failed to reach the top.")
     print(f"Reached altitudes: {', '.join(f'Altitude {i}' for i in range(1, reached + 1))}")
 else:
     print(f"John failed to reach the top.\n"
           f"John didn't reach any altitude.")
-if reached == len_quantity:
-    print("John has reached all the altitudes and managed to reach the top!")
